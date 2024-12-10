da82c000e8e33a6aa22aeac491e70a6f539d3af1
Hamdi Dridi <hadridi365@gmail.com>
2024-12-10 15:57:52 +0100

    new class planning + functions in Gestionnaire + functions in Billet

--- Projet JAVA ESSECT/GESTION SPECTACLE/src/Billet.java
+++ Projet JAVA ESSECT/GESTION SPECTACLE/src/Billet.java
@@ -1,17 +1,21 @@
 public class Billet {
     private static int id_billet = 16845;
     private float prix;
-    private Diffusion  id_diffusion;
+    private Diffusion  id_dif;
 
     public Billet (float prix,Diffusion id_diffusion)
     {
         this.prix=prix;
-        this.id_diffusion=id_diffusion;
+        this.id_dif=id_diffusion;
         id_billet++;
     }
 
     public void affichebillet()
     {
-
+        System.out.println("********************************\n"+
+                           "Billet num : "+id_billet+
+                           "\nprix : "+prix+
+                           "\n*********************************");
+        id_dif.afficher();
     }
 }
